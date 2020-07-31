45a3846d86034814a3751b7cd35b82b4d15fe13b
Sushant Gaurav <68695162+imsushant12@users.noreply.github.com>
2020-07-31 23:13:35 +0530

    Update stonepaperscissor.c

--- stonepaperscissor.c
+++ stonepaperscissor.c
@@ -1,45 +1,64 @@
-int game(char,char);
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int game(char,char);                    //Declaration of the function used in the program.
 main()
 {
-    int n;
-    char you,comp,result;
-    srand(time(0));
-    n=rand()%100;
-    if(n<33)
-        comp='s';
+    int n;                              //n will receive the random number from computer
+    char you,computer,result;           //you denotes user's input and computer denotes guess of computer.Result will contain the final result of the game.
+
+    srand(time(0));                     //srand function will choose random number every time.
+    n=rand()%100;                       //to make the random number between 1 and 100, it is divided by 100
+
+    if(n<33)                            //using simple probability 100 is roughly divided among stone,paper and scissor
+        computer='s';                   //s is for stone
+
     else if(n>33 && n<66)
-        comp='p';
+        computer='p';                   //p is for paper
+
     else
-        comp='z';
-    printf("Enter s for stone, p for paper and z for scissor\n");
-    scanf("%c",&you);
-    result=game(you,comp);
+        computer='z';                   //z is for scissor
+
+    printf("\n\n\n\n\t\t\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
+
+    scanf("%c",&you);                    //input from the user
+
+    result=game(you,computer);           //calling function "game" which will return 1 if user wins,0 if draw and -1 if loss.
+
     if(result==0)
-        printf("Game Draw!\n");
+        printf("\n\n\t\t\t\tGame Draw!\n");
+
     else if(result==1)
-        printf("You have won the game!\n");
+        printf("\n\n\t\t\t\tWow! You have won the game!\n");
+
     else
-        printf("You have lost the game!\n");
-    printf("YOu choose : %c and Computer choose : %c\n",you,comp);
+        printf("\n\n\t\t\t\tOh! You have lost the game!\n");
+
+    printf("\t\t\t\tYOu choose : %c and Computer choose : %c\n",you,computer);
 
 }
-int game(char you,char comp)
+int game(char you,char computer)       //Definition of the function.
 {
-    if(you==comp)
+    if(you==computer)                  //if both user and computer has chose the same thing.
         return 0;
 
-    if(you=='s'&& comp=='p')
-        return -1;
-    else if(you=='p'&& comp=='s')
-        return 1;
-
-    if(you=='s'&& comp=='z')
-        return 1;
-    else if(you=='z'&& comp=='s')
+    if(you=='s'&& computer=='p')       //user's choice is stone and computer's choice is paper.
         return -1;
 
-    if(you=='p'&& comp=='z')
+    else if(you=='p'&& computer=='s')   //user's choice is paper and computer's choice is stone.
+        return 1;
+
+    if(you=='s'&& computer=='z')        //user's choice is stone and computer's choice is scissor.
+        return 1;
+
+    else if(you=='z'&& computer=='s')   //user's choice is scissor and computer's choice is stone.
         return -1;
-    else if(you=='z'&& comp=='p')
+
+    if(you=='p'&& computer=='z')        //user's choice is paper and computer's choice is scissor.
+        return -1;
+
+    else if(you=='z'&& computer=='p')   //user's choice is scissor and computer's choice is paper.
         return 1;
 }
+
