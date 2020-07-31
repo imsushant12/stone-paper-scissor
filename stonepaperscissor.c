fdabe6c1e828a3fe3c3b8ffd79d6d9ad77176ab1
Sushant Gaurav <68695162+imsushant12@users.noreply.github.com>
2020-07-31 23:27:07 +0530

    code of the game (.c file)

--- stonepaperscissor.c
+++ stonepaperscissor.c
@@ -3,22 +3,23 @@
 #include<math.h>
 
 int game(char,char);                    //Declaration of the function used in the program.
-main()
+
+void main()
 {
-    int n;                              //n will receive the random number from computer
-    char you,computer,result;           //you denotes user's input and computer denotes guess of computer.Result will contain the final result of the game.
+    int n;                              //n will receive the random number from computer.
+    char you,computer,result;           //you denotes user's input and computer denotes choice of computer. Result will contain the final result of the game.
 
     srand(time(0));                     //srand function will choose random number every time.
-    n=rand()%100;                       //to make the random number between 1 and 100, it is divided by 100
+    n=rand()%100;                       //to make the random number under 100, divided it by 100.
 
-    if(n<33)                            //using simple probability 100 is roughly divided among stone,paper and scissor
-        computer='s';                   //s is for stone
+    if(n<33)                            //using simple probability 100 is roughly divided among stone,paper and scissor.
+        computer='s';                   //s is denoting stone.
 
     else if(n>33 && n<66)
-        computer='p';                   //p is for paper
+        computer='p';                   //p is denoting paper.
 
     else
-        computer='z';                   //z is for scissor
+        computer='z';                   //z is denoting scissor.
 
     printf("\n\n\n\n\t\t\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
 
@@ -38,12 +39,13 @@ main()
     printf("\t\t\t\tYOu choose : %c and Computer choose : %c\n",you,computer);
 
 }
-int game(char you,char computer)       //Definition of the function.
+
+int game(char you,char computer)        //Definition of the function.
 {
-    if(you==computer)                  //if both user and computer has chose the same thing.
+    if(you==computer)                   //if both user and computer has chose the same thing.
         return 0;
 
-    if(you=='s'&& computer=='p')       //user's choice is stone and computer's choice is paper.
+    if(you=='s'&& computer=='p')        //user's choice is stone and computer's choice is paper.
         return -1;
 
     else if(you=='p'&& computer=='s')   //user's choice is paper and computer's choice is stone.
